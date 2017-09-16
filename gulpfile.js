6509486bc8d5051e561433d05799c87833d886dd
Mauricio Faustino <mauricio.faustino@gmail.com>
2017-09-16 10:49:27 -0300

    adding bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 "use strict";
 
 const gulp = require('gulp');
-const connect =  require('gulp-connect'); //Runs a local dev server
-const open = require('gulp-open'); //Open a URL in a web browser
+const connect =  require('gulp-connect'); // Runs a local dev server
+const open = require('gulp-open'); // Open a URL in a web browser
+const browserify = require('browserify'); // Bundles JS
+const reactify = require('reactify'); // Transforms React JSX to JS
+const source = require('vinyl-source-stream'); // Use conventional text streams with Gulp
+const concat = require('gulp-concat'); // Concatenates files
 
 
 const config = {
@@ -10,7 +14,13 @@ const config = {
     devBaseUrl: 'http://localhost',
     paths: {
         html: './src/*.html',
-        dist: './dist'
+        js: './src/**/*.js',
+        css: [
+            'node_modules/bootstrap/dist/css/bootstrap.min.css',
+            'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
+        ],
+        dist: './dist',
+        mainJs: './src/main.js'
     }
 };
 
@@ -19,8 +29,8 @@ gulp.task('connect', function() {
     connect.server({
         root: ['dist'],
         port: config.port,
-        base: config.deveBaseUrl,
-        liveredload: true
+        base: config.devBaseUrl,
+        livereload: true
     });
 });
 
@@ -35,8 +45,25 @@ gulp.task('html', function() {
         .pipe(connect.reload());
 });
 
-gulp.task('watch', function() {
-    gulp.watch(config.paths.html, ['html']);
+gulp.task('js', function() {
+    browserify(config.paths.mainJs)
+        .transform(reactify)
+        .bundle()
+        .on('error', console.error.bind(console))
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest(config.paths.dist + '/scripts'))
+        .pipe(connect.reload());
 });
 
-gulp.task('default', ['html', 'open', 'watch']);
+gulp.task('css', function() {
+    gulp.src(config.paths.css)
+        .pipe(concat('bundle.css'))
+        .pipe(gulp.dest(config.paths.dist + '/css'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch(config.paths.html, ['html']);
+    gulp.watch(config.paths.js, ['js']);
+});
+
+gulp.task('default', ['html', 'js', 'css', 'open', 'watch']);
--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+$ = jQuery = require('jquery');
+
 var App = console.log('Hello world from Browserfy');
 
 module.export = App;
