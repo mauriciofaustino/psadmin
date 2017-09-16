4258893edf9e7542fdfbc73bb06b1d4cea6593a8
Mauricio Faustino <mauricio.faustino@gmail.com>
2017-09-16 12:38:39 -0300

    adding first component

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 $ = jQuery = require('jquery');
+var React = require("react");
+var Home = require("./components/homePage");
 
-var App = console.log('Hello world from Browserfy');
-
-module.export = App;
+React.render(<Home />, document.getElementById('app'));
