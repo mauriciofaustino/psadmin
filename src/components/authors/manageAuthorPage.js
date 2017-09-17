3d28ebf48989fdbc973f1091c5e7639ccfc5dfa7
Mauricio Faustino <mauricio.faustino@gmail.com>
2017-09-17 09:21:57 -0300

    created save flow + adding toastr

--- src/components/authors/manageAuthorPage.js
+++ src/components/authors/manageAuthorPage.js
@@ -1,9 +1,16 @@
 "use strict";
 
 var React = require('react');
+var Router = require('react-router');
 var AuthorForm = require('./authorForm');
+var AuthorApi = require('../../api/authorApi');
+var Toastr = require('toastr');
 
 var ManageAuthorPage = React.createClass({
+    mixins: [
+        Router.Navigation
+    ],
+
     getInitialState: function() {
       return {
           author: {
@@ -21,11 +28,19 @@ var ManageAuthorPage = React.createClass({
       return this.setState({author: this.state.author});
     },
 
+    saveAuthor: function(event) {
+        event.preventDefault();
+        AuthorApi.saveAuthor(this.state.author);
+        Toastr.success('Author saved.');
+        this.transitionTo('authors');
+    },
+
    render: function() {
        return (
             <AuthorForm
                 author={this.state.author}
-                onChange={this.setAuthorState} />
+                onChange={this.setAuthorState}
+                onSave={this.saveAuthor}/>
        );
    }
 });
